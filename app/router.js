882ac48b0e69cbf40b901378c7feb339d4853ddd
Ryan M <rm.main@gmail.com>
2016-01-12 12:20:30 -0600

    Basic Ember App with Routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,20 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  //These are just standard routes.
+  this.route('about');
+  this.route('services');
+  this.route('contact');
+
+  /*This creates a route for posts and looks at the posts folder
+  in templates/posts/ and looks for the index.hbs file.  After that
+  it looks for the inner-route of "new".  It will also look for anything
+  that is listed after /posts that isn't /new.  Ex. /posts/1 */
+
+  this.resource('posts', {path: '/posts'}, function(){
+      this.route('new');
+      this.route('post', {path: ':post_id'});
+  });
 });
 
 export default Router;
